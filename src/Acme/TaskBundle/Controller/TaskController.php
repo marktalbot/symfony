5e12dbf9ca3665649f9ccf17644ded66d1aa4c4a
Mark Talbot <markjtalbot@gmail.com>
2016-08-01 15:16:00 -0400

    Adding functionality which posts form to persist new task in database

--- src/Acme/TaskBundle/Controller/TaskController.php
+++ src/Acme/TaskBundle/Controller/TaskController.php
@@ -3,19 +3,63 @@
 namespace Acme\TaskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Acme\TaskBundle\Entity\Task;
+use Acme\TaskBundle\Form\TaskType;
 
 class TaskController extends Controller
 {
 	public function indexAction()
 	{
-		return $this->render('AcmeTaskBundle:Tasks:index.html.twig');
+		$task = new Task;
+
+		$form = $this->createForm(new TaskType, $task, [
+			'action' => $this->generateUrl('acme_store_task'),
+			'method' => 'POST',
+		]);
+
+		$form->add('submit', 'submit', [
+			'label' => 'Add Task',
+			'attr'  => ['class' => 'btn btn-default'],
+		]);
+
+		return $this->render('AcmeTaskBundle:Tasks:index.html.twig', [
+			'form' => $form->createView(),
+		]);
 	}
 
-	public function storeAction()
+	public function storeAction(Request $request)
 	{
-		echo '<pre>';
-		print_r('post here to store task in db');
-		echo '</pre>';exit;	
+		$task = new Task;
+
+		$form = $this->createForm(new TaskType, $task, [
+			'action' => $this->generateUrl('acme_store_task'),
+			'method' => 'POST',
+		]);
+
+		$form->add('submit', 'submit', [
+			'label' => 'Add Task',
+			'attr'  => ['class' => 'btn btn-default'],
+		]);
+
+		$form->handleRequest($request);
+		
+		if ($form->isValid()) {
+			
+			$em = $this->getDoctrine()->getManager();
+
+			$em->persist($task);
+			$em->flush();
+
+			$this->get('session')->getFlashBag()->add('success', 'The task has been created');
+
+			return $this->redirect($this->generateUrl('acme_tasks'));
+
+		}
+
+		$this->get('session')->getFlashBag()->add('error', 'Error: something whent wrong');
+
+		return $this->redirect($this->generateUrl('acme_tasks'));
 	}
 
 	public function destroyAction($id)
---
+++ src/Acme/TaskBundle/Form/TaskType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Acme\TaskBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\OptionsResolverInterface;
+
+class TaskType extends AbstractType
+{
+	
+	public function buildForm(FormBuilderInterface $builder, array $optons)
+	{
+		$builder->add('title', null, [
+			'label' => 'Task',
+			'attr' => ['class' => 'form-control'],
+		]);
+	}
+
+	public function setDefaults(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults([
+			'data_class' => 'Acme\TaskBundle\Entity\Task',
+		]);
+	}
+
+	public function getName()
+	{
+		return 'acme_taskbundle_task';
+	}
+}
